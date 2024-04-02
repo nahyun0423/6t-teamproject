7528577cc1ff5bb5246da44001670af763b95847
SeongHwaan <k99302133@gmail.com>
2024-04-02 18:33:44 +0900

    feat: change compose to xml

--- app/src/main/java/com/team6/routineapp/MainActivity2.kt
+++ app/src/main/java/com/team6/routineapp/MainActivity2.kt
@@ -3,9 +3,9 @@ package com.team6.routineapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2) //xml과 연결
     }
 }
