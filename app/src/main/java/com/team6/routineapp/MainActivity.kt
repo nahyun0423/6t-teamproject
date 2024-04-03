5d132f447f6c96b26518ffaf01d87d4dd15d5659
5-grains <116898750+5-grains@users.noreply.github.com>
2024-04-03 21:48:31 +0900

    1
    1

--- app/src/main/java/com/team6/routineapp/MainActivity.kt
+++ app/src/main/java/com/team6/routineapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.team6.routineapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonToRecommendationResult = findViewById<Button>(R.id.buttonToRecommendationResult)
+
+        buttonToRecommendationResult.setOnClickListener{
+            val intent = Intent(this, RecommendationResultActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/team6/routineapp/RecommendationResultActivity.kt
@@ -0,0 +1,12 @@
+package com.team6.routineapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class RecommendationResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recommendation_result)
+    }
+}
