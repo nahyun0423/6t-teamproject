2c74bee5d46ce3da655debb3c5bbd581979d928c
5-grains <116898750+5-grains@users.noreply.github.com>
2024-04-04 17:36:04 +0900

    2
    2

--- app/src/main/java/com/team6/routineapp/RecommendationResultActivity.kt
+++ app/src/main/java/com/team6/routineapp/RecommendationResultActivity.kt
@@ -3,10 +3,15 @@ package com.team6.routineapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.RelativeLayout
 
 class RecommendationResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recommendation_result)
+
+        for (i : Int in 1..6) {
+            val routine = RelativeLayout(this, )
+        }
     }
 }
