868f3f12e8e9d91edff8b69c2b200e795ef3113a
SeongHwaan <k99302133@gmail.com>
2024-04-08 23:47:09 +0900

    feat: modify constraint

--- app/src/main/java/com/team6/routineapp/ChoiceTap.kt
+++ app/src/main/java/com/team6/routineapp/ChoiceTap.kt
@@ -3,9 +3,9 @@ package com.team6.routineapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity2 : AppCompatActivity() {
+class ChoiceTap : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2) //xml과 연결
+        setContentView(R.layout.choicetap)
     }
 }
