de750530306703316847131b9a435b1ae27dc5b0
5-grains <116898750+5-grains@users.noreply.github.com>
2024-04-20 16:55:14 +0900

    1

--- app/src/main/java/com/team6/routineapp/SelectExerciseActivity.kt
+++ app/src/main/java/com/team6/routineapp/SelectExerciseActivity.kt
@@ -1,52 +1,30 @@
 package com.team6.routineapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.TypedValue
-import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-open class Exercise(var name: String, var set: Int, var numberOfTimes: Int, var part: String, var tool: String) {
-    open fun getDetail(): String {
-        return String.format("%d회, %d세트", numberOfTimes, set);
-    }
-}
-
-class WeightTraining(name: String, set: Int, numberOfTimes: Int, part: String, tool: String, var weight: Int) :
-    Exercise(name, set, numberOfTimes, part, tool) {
-    override fun getDetail(): String {
-        return String.format("%dkg, ", weight) + super.getDetail()
-    }
-}
-
-class RecommendationResultActivity : AppCompatActivity() {
+class SelectExerciseActivity : AppCompatActivity() {
     fun convertFromDpToPx(value: Int): Int {
         var displayMetrics = resources.displayMetrics
         var result = Math.round(value * displayMetrics.density)
 
         return result;
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recommendation_result)
+        setContentView(R.layout.activity_select_exercise)
 
-        val intentToRoutineList = Intent(this, RoutineList::class.java)
         val intentToDetail = Intent(this, DetailInfoActivity::class.java)
 
-        val buttonAddToRoutine = findViewById<RelativeLayout>(R.id.button_2)
-        buttonAddToRoutine.setOnClickListener{
-            startActivity(intentToRoutineList)
-        }
-
-        //이미지도 추가?
         val exercise1 = WeightTraining("오버헤드 프레스", 4, 10, "팔", "바벨", 40)
         val exercise2 = Exercise("행잉 레그 레이즈", 4, 10, "코어", "행잉 레그 레이즈 머신")
         val exercise3 = Exercise("행잉 레그 레이즈", 4, 10, "코어", "행잉 레그 레이즈 머신")
@@ -55,7 +33,7 @@ class RecommendationResultActivity : AppCompatActivity() {
         val exercise6 = Exercise("행잉 레그 레이즈", 4, 10, "코어", "행잉 레그 레이즈 머신")
         val routine = arrayOf(exercise1, exercise2, exercise3, exercise4, exercise5, exercise6)
 
-        val middle = findViewById<LinearLayout>(R.id.middle)
+        val middle = findViewById<LinearLayout>(R.id.select_exercise_middle)
         var exercise: RelativeLayout
         var icon: ImageView
         var name: TextView
@@ -76,10 +54,10 @@ class RecommendationResultActivity : AppCompatActivity() {
             detail = TextView(this)
 
             parameter = RelativeLayout.LayoutParams(convertFromDpToPx(300), convertFromDpToPx(80))
-            parameter.setMargins(0, convertFromDpToPx(30), 0, 0)
+            parameter.setMargins(30, convertFromDpToPx(30), 0, 0)
             exercise.layoutParams = parameter
             exercise.setOnClickListener{
-                startActivity(intentToDetail)
+
             }
 
             parameter = RelativeLayout.LayoutParams(convertFromDpToPx(80), convertFromDpToPx(80))
@@ -92,24 +70,10 @@ class RecommendationResultActivity : AppCompatActivity() {
             name.setTextAppearance(R.style.exercise_name)
             name.setText(i.name)
 
-            parameter = RelativeLayout.LayoutParams(convertFromDpToPx(200), convertFromDpToPx(20))
-            parameter.setMargins(convertFromDpToPx(100), convertFromDpToPx(40), 0, 0)
-            detail.layoutParams = parameter
-            detail.setTextAppearance(R.style.exercise_detail)
-            detail.setText(i.getDetail())
-
             exercise.addView(icon)
             exercise.addView(name)
             exercise.addView(detail)
             middle.addView(exercise)
         }
-
-        val blankSpace = RelativeLayout(this)
-        parameter = RelativeLayout.LayoutParams(convertFromDpToPx(300), convertFromDpToPx(180))
-        parameter.setMargins(0, 0, 0, convertFromDpToPx(30))
-        blankSpace.layoutParams = parameter
-        middle.addView(blankSpace)
-
-
     }
 }
