60b7fff7ddb6286cfece2a5c17f7ca28f779130e
B1ueccc <105410278+B1ueccc@users.noreply.github.com>
2024-05-20 18:25:34 +0900

    test and modify

--- CapAndroid/android/app/src/main/java/com/team6/routineapp/RegisterActivity.kt
+++ CapAndroid/android/app/src/main/java/com/team6/routineapp/RegisterActivity.kt
@@ -3,7 +3,7 @@ package com.team6.routineapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Register : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
